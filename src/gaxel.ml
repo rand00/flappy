fc469d9f0f7e17b3625d38b45d9569ef857905e0
rand00 <oth.rand@gmail.com>
2018-11-25 18:09:39 +0100

    Rewrote to use reactive tyxml_jsoo instead of manual removal/add

--- src/gaxel.ml
+++ src/gaxel.ml
@@ -47,6 +47,7 @@
 
 open Lwt.Infix
 open Lwt_react
+open ReactiveData
 open Gg
 module H = Tyxml_js.Html
 module R = Tyxml_js.R.Html
@@ -55,47 +56,16 @@ let tick_e, tick_eupd = E.create ()
 let fps = 30.
 let game_node_id = "gaxel"
 
-module Renderable = struct 
+module Signal = struct
 
-  type t = {
-    image : string;
-    pos_x : int;
-    pos_y : int;
-    width : int;
-    height : int;
-  }
+  module Infix = struct 
   
+    let (>>=) s f = S.bind s f
+    let (>>|) s f = S.map f s 
+
+  end
+    
 end
-
-open Renderable
-
-let feed_frp ()=
-  let rec loop frame =
-    tick_eupd frame;
-    Lwt_js.sleep (1. /. fps) >>= fun () ->
-    loop (succ frame)
-  in
-  Lwt.async @@ (fun () -> loop 0)
-
-let circle_pos_s : (float * float) S.t =
-  let circle frame =
-    let t = (float frame *. 2. *. Float.pi) /. fps in
-    sin t, cos t
-  in
-  E.map circle tick_e
-  |> S.hold (0., 0.)
-
-let circle_01_s =
-  circle_pos_s
-  |> S.map (fun (pos_x, pos_y) -> {
-        image = "https://beautifulcoolwallpapers.files.wordpress.com/\
-                 2011/09/the-best-top-desktop-horse-wallpapers-21.jpg";
-        pos_x = truncate (pos_x *. 100.); (*goto*)
-        pos_y = truncate (pos_y *. 100.);
-        width = 1000;
-        height = 1000;
-      }
-    )
 
 module JsAux = struct
 
@@ -109,36 +79,68 @@ end
 
 let sp = Printf.sprintf
 
-(*goto
-  . make git commit for this version
-  . make new branch that tries to use reactive tyxml
+let feed_frp ()=
+  let rec loop frame =
+    tick_eupd frame;
+    Lwt_js.sleep (1. /. fps) >>= fun () ->
+    loop (succ frame)
+  in
+  Lwt.async @@ (fun () -> loop 0)
+
+let circle_pos_s : (float * float) S.t =
+  let circle frame =
+    let t = (float frame *. 0.5 *. Float.pi) /. fps in
+    sin t, cos t
+  in
+  E.map circle tick_e
+  |> S.hold (0., 0.)
+
+(*goto brian; how to practically make collision detection etc. here; 
+  > need to have all relevant info available for this calculation
+    > so either return both relevant info + component based on this
+      > or better yet; define some function from 'entity' to 'component' 
+        @brian; what to have in common between entities, and what to define as 
+        sumtypes for different rendering?
 *)
+
+let circle_01_s =
+  let style_s =
+    circle_pos_s
+    |> S.map (fun (pos_x, pos_y) ->
+        String.concat "" [
+          "position: fixed;";
+          sp "background-image: url(%s);"
+            "https://beautifulcoolwallpapers.files.wordpress.com/\
+             2011/09/the-best-top-desktop-horse-wallpapers-21.jpg";
+          sp "width: %dpx;" 1000;
+          sp "height: %dpx;" 1000;
+          sp "left: %dpx;" (truncate (pos_x *. 100.));
+          sp "top: %dpx;" (truncate (pos_y *. 100.));
+        ]
+      )
+  in
+  H.div ~a:[ R.a_style style_s ] []
+
+(*goto make this list dynamic, based on 'alive/dead'*)
+let components_s : Html_types.body_content H.elt list S.t =
+  S.const [ 
+    circle_01_s 
+  ]
+
 let _render =
-  circle_01_s |> S.map (fun renderable ->
-      let open CCOpt.Infix in
-      let root = Dom_html.getElementById game_node_id in
-      JsAux.remove_children root;
-      let renderable_node =
-        H.div ~a:[
-          H.a_style @@ String.concat "" [
-            sp "background-image: url(%s);" renderable.image;
-            "position: fixed;";
-            sp "width: %dpx;" renderable.width;
-            sp "height: %dpx;" renderable.height;
-            sp "left: %dpx;" renderable.pos_x;
-            sp "top: %dpx;" renderable.pos_y;
-          ]
-        ] []
-        |> Tyxml_js.To_dom.of_node
-      in
-      root##appendChild renderable_node
-    )
+  let root = Dom_html.getElementById game_node_id in
+  let reactive_child =
+    components_s
+    |> RList.from_signal
+    |> R.div ~a:[] 
+    |> Tyxml_js.To_dom.of_node
+  in
+  root##appendChild reactive_child
 
 let main () =
   Dom_html.window##.onload := Dom_html.handler (fun _ -> 
-      (* tick_eupd 0; (\*goto remove debug*\) *)
+      (* tick_eupd 0; (\*for debug*\) *)
       feed_frp ();
-      Printf.printf "FOOO\n";
       Js._false
     )
 
